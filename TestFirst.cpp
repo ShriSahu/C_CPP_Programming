7873e9a102bd5b44e47875845255a320c1f0f0dc
ShriSahu <sahushrikant.sgsits@gmail.com>
2023-10-03 19:36:43 +0200

    basics

--- TestFirst.cpp
+++ TestFirst.cpp
@@ -1,8 +1,57 @@
-#include<iostream>
-using namespace std;
-int main ()
-{
-    cout<<"My First Program"<<endl;
-    cout<<"Updated"<<endl;
-    return 0;
-}
+//Area of Triangle
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	float a, b, h;
+//	cout << "Please enter Base & Height" << endl;
+//	cin >> b >> h;
+//	a = (b * h) / 2;
+//	cout << "Aria is " << a << endl;
+//
+//	return 0;
+//}
+
+//******Finding sum of first N natutal numbers ******************
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int sum, n;
+//	cout << "Please enter N value" << endl;
+//	cin >> n;
+//	sum = (n * (n + 1)) / 2;
+//	cout << "Sum of first " << n << "natural numbers are : " << sum;
+//	return 0;
+//}
+
+//******Finding roots of quadratic equation and Area of Circle ******************
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, c;
+//	float radius, area;
+//
+//	int root1, root2;
+//
+//	cout << "please enter values for a, b and c in a*x*x+ b*x +c " << endl,
+//	cin >> a >> b >> c,
+//
+//	root1 = (-b + sqrt(b * b + 4 * a * c)) / (2 * a);
+//	root2 = (-b - sqrt(b * b + 4 * a * c)) / (2 * a);
+//
+//	cout << "roots are : " << root1 << " " << root2 << endl;
+//
+//	cout << " enter radius" << endl;
+//	cin >> radius;
+//	area = (float)22 / 7 * radius * radius;
+//	cout << " area of the circle is : " << area << endl;
+//
+//	return 0;
+//
+//}
+
