4d8e9d7c04fdc6fa755c0662859ef6cf264c673c
ShriSahu <sahushrikant.sgsits@gmail.com>
2023-10-10 20:42:29 +0200

    10Oct

--- BasicsTryOut/BasicsTryOut/Basics.cpp
+++ BasicsTryOut/BasicsTryOut/Basics.cpp
@@ -55,16 +55,159 @@
 //
 //}
 
-//*********Bitwise operator*********************
+////*********Bitwise operator*********************
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	char x = 10;
+//
+//	int y = (int )x << 2;
+//
+//	cout << y << endl;
+//
+//    return 0;
+//}
+
+////*********enum and typedef*********************
+//#include<iostream>
+//using namespace std;
+//
+//enum day {mon, tue, wed, thu, fri, sat , sun};
+//enum department { cs=2, it, ece};
+//typedef int marks;
+//int main()
+//{
+//	day d = tue;
+//	department dp1 = it, dp2 = ece;
+//
+//	cout << d << endl;
+//
+//	day d2 = sun;
+//
+//	cout << d2 << endl;
+//
+//	cout << dp1 << endl;
+//	cout << dp2 << endl;
+//
+//	marks m1, m2;
+//	m1 = 100;
+//	m2 = 200;
+//
+//	cout << m1 << " "<<m2 << endl;
+//
+//	return 0;
+//
+//}
+////*********Conditional Statement*********************
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	float a, b, c;
+//
+//	cout << "enter  and b" << endl;
+//	cin >> a >> b;
+//
+//	if (b == 0)
+//	{
+//		cout << "devided by zero not allowed" << endl;
+//		return 0;
+//	}
+//		
+//	else
+//	{
+//		c = a / b;
+//		return 0;
+//	}
+//		
+//}
+
+////*********Conditional Statement Finding Max of 3 no *********************
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, c;
+//
+//	cout << "enter  a , b and c" << endl;
+//	cin >> a >> b >> c;
+//
+//	if (a > b && a > c)
+//		cout << "a is maximum no" << endl;
+//	else if (b > c)
+//		cout << "b is maximum no" << endl;
+//	else
+//		cout << "c is maximum no" << endl;
+//
+//	return 0;
+//}
+
+///*********Nature of Quadratic roots*****************************
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	float  a, b, c;
+//
+//	cout << "enter  a , b and c" << endl;
+//	cin >> a >> b >> c;
+//
+//	int d = (b * b) - (4 * a * c);
+//
+//	if (d == 0)
+//	{
+//		int root = -b / (2 * a);
+//		cout << "real and equal roots : " << root << endl;
+//	}
+//	else if (d > 0)
+//	{
+//		float root1 = (-b + sqrt((b * b) - (4 * a * c))) / (2 * a);
+//		float root2 = (-b + sqrt((b * b) - (4 * a * c))) / (2 * a);
+//
+//		cout << "real and unequal roots are : " << root1 << " "<<root2 << endl;
+//	}
+//	else
+//	{
+//		cout << "roots are imaginary" << endl;
+//	}
+//	
+//
+//	return 0;
+//}
+
+
+///*********Student Grades*****************************
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	char x = 10;
+	int  m1, m2, m3;
 
-	int y = (int )x << 1;
+	cout << "enter Marks m1, m2 and m3" << endl;
+	cin >> m1 >> m2 >> m3;
 
-	cout << y << endl;
+	int avgMarks = m1 + m2 + m3 / 3;
 
-    return 0;
+	if (avgMarks >= 65)
+	{
+		
+		cout << "Grade A : " <<endl;
+	}
+	else if ( avgMarks < 65 && avgMarks >=35 )
+	{
+		
+		cout << "Grade B : " << endl;
+	}
+	else
+	{
+		cout << "Grade C" << endl;
+	}
+
+
+	return 0;
 }
